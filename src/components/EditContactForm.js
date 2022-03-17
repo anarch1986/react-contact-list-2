727426d0a3a7910f4b9ee2a087c2ad8ed3ff39d9
tamas.varga5@vodafone.com <tamas.varga5@vodafone.com>
2022-03-17 17:19:54 +0100

    added validation to EditContactForm

--- src/components/EditContactForm.js
+++ src/components/EditContactForm.js
@@ -26,32 +26,85 @@ function EditContactForm(props) {
     },
   });
 
-  const firstNameInputRef = useRef()
-  const lastNameInputRef = useRef()
-  const emailInputRef = useRef()
-  const phoneInputRef = useRef()
+  const [isFirstNameError, setIsFirstNameError] = useState(false);
+  const [isLastNameError, setIsLastNameError] = useState(false);
+  const [isEmailError, setIsEmailError] = useState(false);
+  const [isPhoneError, setIsPhoneError] = useState(false);
 
+  function validateFiled(field) {
+    if (field && field !== "" && field.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function validateEmail(email) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  }
+
+  const firstNameInputRef = useRef();
+  const lastNameInputRef = useRef();
+  const emailInputRef = useRef();
+  const phoneInputRef = useRef();
 
   useEffect(() => {
     if (props.contact) {
-      console.log(props.contact)
-      firstNameInputRef.current.value = props.contact.name.first
-      lastNameInputRef.current.value = props.contact.name.last
-      emailInputRef.current.value = props.contact.email
-      phoneInputRef.current.value = props.contact.phone
+      firstNameInputRef.current.value = props.contact.name.first;
+      lastNameInputRef.current.value = props.contact.name.last;
+      emailInputRef.current.value = props.contact.email;
+      phoneInputRef.current.value = props.contact.phone;
       setContact(props.contact);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.contact]);
 
-  function handleOnClick() {
-    contact.name.first = firstNameInputRef.current.value
-    contact.name.last = lastNameInputRef.current.value
-    contact.email = emailInputRef.current.value
-    contact.phone = phoneInputRef.current.value
-    props.handleSaveContact(contact)
+  function validateForm() {
+    let isValid = true
+    if (validateFiled(firstNameInputRef.current.value)) {
+      contact.name.first = firstNameInputRef.current.value;
+      setIsFirstNameError(false);
+    } else {
+      setIsFirstNameError(true);
+      isValid = false
+    }
+
+    if (validateFiled(lastNameInputRef.current.value)) {
+      contact.name.last = lastNameInputRef.current.value;
+      setIsLastNameError(false);
+    } else {
+      setIsLastNameError(true);
+      isValid = false
+    }
+
+    if (validateEmail(emailInputRef.current.value)) {
+      contact.email = emailInputRef.current.value;
+      setIsEmailError(false);
+    } else {
+      setIsEmailError(true);
+      isValid = false
+    }
+
+    if (validateFiled(phoneInputRef.current.value)) {
+      contact.phone = phoneInputRef.current.value;
+      setIsPhoneError(false);
+    } else {
+      setIsPhoneError(true);
+      isValid = false
+    }
+
+    return isValid
   }
 
+  function handleOnClick() {
+    if (validateForm()) {
+      props.handleSaveContact(contact);
+    }
+  }
 
   return (
     <Container>
@@ -80,9 +133,21 @@ function EditContactForm(props) {
                 css={css`
                   text-align: left;
                 `}
-                type="text" ref={firstNameInputRef}
+                type="text"
+                ref={firstNameInputRef}
               />
+              <br />
             </Form.Group>
+            {isFirstNameError && (
+              <small
+                css={css`
+                  color: ${colors.red};
+                `}
+              >
+                Please provide a valid First Name
+              </small>
+            )}
+
             <Form.Group
               css={css`
                 display: flex;
@@ -102,9 +167,19 @@ function EditContactForm(props) {
                 css={css`
                   text-align: left;
                 `}
-                type="text" ref={lastNameInputRef}
+                type="text"
+                ref={lastNameInputRef}
               />
             </Form.Group>
+            {isLastNameError && (
+              <small
+                css={css`
+                  color: ${colors.red};
+                `}
+              >
+                Please provide a valid Last Name
+              </small>
+            )}
             <Form.Group
               css={css`
                 display: flex;
@@ -124,9 +199,19 @@ function EditContactForm(props) {
                 css={css`
                   text-align: left;
                 `}
-                type="email" ref={emailInputRef}
+                type="email"
+                ref={emailInputRef}
               />
             </Form.Group>
+            {isEmailError && (
+              <small
+                css={css`
+                  color: ${colors.red};
+                `}
+              >
+                Please provide a valid Email
+              </small>
+            )}
             <Form.Group
               css={css`
                 display: flex;
@@ -146,9 +231,19 @@ function EditContactForm(props) {
                 css={css`
                   text-align: left;
                 `}
-                type="tel" ref={phoneInputRef}
+                type="tel"
+                ref={phoneInputRef}
               />
             </Form.Group>
+            {isPhoneError && (
+              <small
+                css={css`
+                  color: ${colors.red};
+                `}
+              >
+                Please provide a valid Phone number
+              </small>
+            )}
             <Button
               variant="outline-dark"
               css={css`
