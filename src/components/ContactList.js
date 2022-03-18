c0a72884cae0824baa2ab81b8a4178739e3b69e9
tamas.varga5@vodafone.com <tamas.varga5@vodafone.com>
2022-03-18 12:31:31 +0100

    Added list animation

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,13 +1,25 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 import ContactCard from "./ContactCard";
 
 function ContactList(props) {
+  const [contactList, setContactList] = useState([]);
+
+  useEffect(() => {
+    setContactList([])
+    setTimeout(() => {
+      setContactList(props.contacts)
+    }, 100)
+  }, [props]);
+
+  
+
   return (
     <Container>
       <Row>
-        {props.contacts.map((contactItem) => (
-          <Col lg={4} key={contactItem.uid}>
+        {contactList.map((contactItem) => (
+          <Col className="fadeIn" lg={4} key={contactItem.uid}>
             <ContactCard contact={contactItem} />
           </Col>
         ))}
